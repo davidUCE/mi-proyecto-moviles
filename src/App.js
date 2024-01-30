05d96ac294827a8ccf17bd0162986f4b9cbfb16a
davidUCE <jdsoria@uce.edu.ec>
2024-01-30 18:07:04 -0500

    cambios de fuente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import FacebookIcon from './Facebook';
+import PerfilC from './Perfil';
 
 function App() {
   return (
     <div>
       <h1>Mis Redes Sociales</h1>
-      <FacebookIcon/>
+      <PerfilC/>
         </div>
   );
 }
